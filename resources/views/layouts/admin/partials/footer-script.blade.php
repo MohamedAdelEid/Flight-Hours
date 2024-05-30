3d04da60abc405877f79a289b7cf23c0256acfde
mohamed adel <01200505348mohamed@gmail.com>
2024-05-31 00:31:19 +0300

    - Add and Handle layouts admin - create index page to dashboard admin ♥

---
+++ resources/views/layouts/admin/partials/footer-script.blade.php
@@ -0,0 +1,14 @@
+
+<!-- script js cdn -->
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+
+<script src="{{ asset('assets/js/admin/init-alpine.js') }}"></script>
+
+{{-- <script src=""></script> --}}
+
+<!-- script chart -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
+
+{{-- <script src="charts-lines.js" defer></script> --}}
+
+{{-- <script src="charts-pie.js" defer></script> --}}
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\front\jobs_categoriecontroller;
 use App\Models\Admin;
 use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,49 +26,49 @@ use Illuminate\Support\Facades\Route;
 //************* */ Front Design *************************
 
 Route::prefix('front')->name('front')->group(function () {
-    Route::get('/', FrontHomecontroller::class)->Middleware('auth')->name('index');
+  Route::get('/', FrontHomecontroller::class)->Middleware('auth')->name('index');
 
-    //----------*/ category----
-    Route::get('/category', [jobs_categoriecontroller::class, 'view_jobs'])->Middleware('auth')->name('show');
-    Route::get('/category/add',[jobs_categoriecontroller::class, 'create'])->Middleware('auth')->name('jobs.create');
-    Route::post('/category/add',[jobs_categoriecontroller::class, 'store'])->Middleware('auth')->name('jobs.store');
-    Route::get('/category/edit',[jobs_categoriecontroller::class, 'edit'])->Middleware('auth')->name('jobs.edit');
+  //----------*/ category----
+  Route::get('/category', [jobs_categoriecontroller::class, 'view_jobs'])->Middleware('auth')->name('show');
+  Route::get('/category/add', [jobs_categoriecontroller::class, 'create'])->Middleware('auth')->name('jobs.create');
+  Route::post('/category/add', [jobs_categoriecontroller::class, 'store'])->Middleware('auth')->name('jobs.store');
+  Route::get('/category/edit', [jobs_categoriecontroller::class, 'edit'])->Middleware('auth')->name('jobs.edit');
 
-    //----------*/ types ----
-    Route::get('/types', [Types_categoriecontroller::class, 'view_types'])->Middleware('auth')->name('view_types');
-    Route::get('/types/add',[Types_categoriecontroller::class, 'create'])->Middleware('auth')->name('types.create');
-    Route::post('/types/add',[Types_categoriecontroller::class, 'store'])->Middleware('auth')->name('types.store');
-    Route::get('/types/edit',[Types_categoriecontroller::class, 'edit'])->Middleware('auth')->name('types.edit');
+  //----------*/ types ----
+  Route::get('/types', [Types_categoriecontroller::class, 'view_types'])->Middleware('auth')->name('view_types');
+  Route::get('/types/add', [Types_categoriecontroller::class, 'create'])->Middleware('auth')->name('types.create');
+  Route::post('/types/add', [Types_categoriecontroller::class, 'store'])->Middleware('auth')->name('types.store');
+  Route::get('/types/edit', [Types_categoriecontroller::class, 'edit'])->Middleware('auth')->name('types.edit');
 
-   //----------*/ Airports ----
-        Route::get('/Airports', [Airports_categoriecontroller::class, 'view_Airports'])->Middleware('auth')->name('view_Airports');
-        Route::get('/Airports/add',[Airports_categoriecontroller::class, 'create'])->Middleware('auth')->name('Airports.create');
-        Route::post('/Airports/add',[Airports_categoriecontroller::class, 'store'])->Middleware('auth')->name('Airports.store');
-        Route::get('/Airports/edit',[Airports_categoriecontroller::class, 'edit'])->Middleware('auth')->name('Airports.edit');
-     
-     //----------*/ employees ---- 
-     Route::get('/Employee', [Employeecontroller::class, 'Employee_index'])->Middleware('auth')->name('Employee.index');
-     Route::get('/Employee/view', [Employeecontroller::class, 'Employee_search'])->Middleware('auth')->name('Employee_search');
+  //----------*/ Airports ----
+  Route::get('/Airports', [Airports_categoriecontroller::class, 'view_Airports'])->Middleware('auth')->name('view_Airports');
+  Route::get('/Airports/add', [Airports_categoriecontroller::class, 'create'])->Middleware('auth')->name('Airports.create');
+  Route::post('/Airports/add', [Airports_categoriecontroller::class, 'store'])->Middleware('auth')->name('Airports.store');
+  Route::get('/Airports/edit', [Airports_categoriecontroller::class, 'edit'])->Middleware('auth')->name('Airports.edit');
 
-    //----------*/ login  ---------
-    Route::view ('/login','front.auth.login');
-    Route::view ('/register','front.auth.register');
-    Route::view ('/forgot','front.auth.forgotpassword');
+  //----------*/ employees ---- 
+  Route::get('/Employee', [Employeecontroller::class, 'Employee_index'])->Middleware('auth')->name('Employee.index');
+  Route::get('/Employee/view', [Employeecontroller::class, 'Employee_search'])->Middleware('auth')->name('Employee_search');
+
+  //----------*/ login  ---------
+  Route::view('/login', 'front.auth.login');
+  Route::view('/register', 'front.auth.register');
+  Route::view('/forgot', 'front.auth.forgotpassword');
 
 });
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 //***************** +++++++++++++++++++++++++++++++++++++++++++++++++++++ ******************
 
 //***************** */ Admin Design ******************
 
 Route::prefix('admin')->name('admin')->group(function () {
-   Route::get('/', backHomecontroller::class)->Middleware('admin')->name('index');
-   
-  Route::view ('/login','back.auth.login');
-  Route::view ('/register','back.auth.register');
-  Route::view ('/forgot','back.auth.forgotpassword');
-  require __DIR__.'/Adminauth.php';
+  Route::get('/', backHomecontroller::class)->Middleware('admin')->name('index');
+
+  Route::view('/login', 'back.auth.login');
+  Route::view('/register', 'back.auth.register');
+  Route::view('/forgot', 'back.auth.forgotpassword');
+  require __DIR__ . '/Adminauth.php';
 });
 
 //***************** +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++******************
@@ -75,18 +76,19 @@ Route::prefix('admin')->name('admin')->group(function () {
 //***************** */ Captin Design ******************
 
 Route::prefix('Captin')->name('Captin')->group(function () {
-  Route::get('/',CaptinHomecontroller::class)->Middleware('captin')->name('index');
-  Route::view ('/login','back.auth.login');
- Route::view ('/register','back.auth.register');
- Route::view ('/forgot','back.auth.forgotpassword');
- require __DIR__.'/Captinauth.php';
+  Route::get('/', CaptinHomecontroller::class)->Middleware('captin')->name('index');
+  Route::view('/login', 'back.auth.login');
+  Route::view('/register', 'back.auth.register');
+  Route::view('/forgot', 'back.auth.forgotpassword');
+  require __DIR__ . '/Captinauth.php';
 
 });
 
 Route::get('/', function () {
- return view('welcome');
+  return view('welcome');
 });
 
 
-
-
+Route::get('/dashboard-admin', function () {
+  return view('admin.index');
+});
